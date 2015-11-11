80a8628bb5aeb61f186ade3a8abafe7c0421e3ff
dianfu <dian.fu@intel.com>
2015-11-11 16:49:18 +0800

    Bugfix: StreamInput should return -1 to the caller when the underline inputstream return -1

--- src/main/java/com/intel/chimera/StreamInput.java
+++ src/main/java/com/intel/chimera/StreamInput.java
@@ -1,3 +1,20 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.intel.chimera;
 
 import java.io.IOException;
@@ -22,6 +39,9 @@ public class StreamInput implements Input {
     while (remaining > total) {
       final int n = in.read(tmp, 0, Math.min(remaining, bufferSize));
       if (n == -1) {
+        if (total == 0) {
+          total = -1;
+        }
         break;
       } else if (n > 0) {
         dst.put(tmp, pos, n);
