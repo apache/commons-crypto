d24ee6bec33a08e0dcc3b720d1220beccf47941d
Junjie Chen <junjie.j.chen@intel.com>
2016-10-19 02:34:42 +0800

    [CRYPTO-125] CryptoOutputStream does not call write in a loop when underlying channel works in non-block mode.

--- src/main/java/org/apache/commons/crypto/stream/CryptoOutputStream.java
+++ src/main/java/org/apache/commons/crypto/stream/CryptoOutputStream.java
@@ -360,7 +360,9 @@ public class CryptoOutputStream extends OutputStream implements
         outBuffer.flip();
 
         // write to output
-        output.write(outBuffer);
+        while (outBuffer.hasRemaining()) {
+            output.write(outBuffer);
+        }
     }
 
     /**
