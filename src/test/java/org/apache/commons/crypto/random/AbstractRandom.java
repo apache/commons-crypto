9b1d1b042d5637e0770f71c64d6d30d0b871dab2
Gary Gregory <garydgregory@gmail.com>
2022-12-12 07:20:40 -0500

    Better name

--- src/test/java/org/apache/commons/crypto/random/AbstractRandom.java
+++ src/test/java/org/apache/commons/crypto/random/AbstractRandom.java
@@ -25,7 +25,7 @@ import java.util.Properties;
 abstract class AbstractRandom implements CryptoRandom {
 
     // Should fail to instantiate, as it is an abstract class
-    AbstractRandom(final Properties props) {
+    AbstractRandom(final Properties properties) {
 
     }
 }
