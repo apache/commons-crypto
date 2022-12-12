e21eadec21da69ebdc7761fc139e6a0b8e44a363
Gary Gregory <garydgregory@gmail.com>
2022-12-12 07:18:41 -0500

    Format

--- src/test/java/org/apache/commons/crypto/random/AbstractRandomTest.java
+++ src/test/java/org/apache/commons/crypto/random/AbstractRandomTest.java
@@ -1,20 +1,20 @@
- /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+/*
+* Licensed to the Apache Software Foundation (ASF) under one
+* or more contributor license agreements.  See the NOTICE file
+* distributed with this work for additional information
+* regarding copyright ownership.  The ASF licenses this file
+* to you under the Apache License, Version 2.0 (the
+* "License"); you may not use this file except in compliance
+* with the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
 package org.apache.commons.crypto.random;
 
 import java.lang.Thread.State;
@@ -29,8 +29,7 @@ import org.junit.jupiter.api.Timeout;
 
 public abstract class AbstractRandomTest {
 
-    public abstract CryptoRandom getCryptoRandom()
-            throws GeneralSecurityException;
+    public abstract CryptoRandom getCryptoRandom() throws GeneralSecurityException;
 
     @Test
     @Timeout(value = 120000, unit = TimeUnit.MILLISECONDS)
@@ -74,8 +73,7 @@ public abstract class AbstractRandomTest {
     }
 
     /**
-     * Test will timeout if secure random implementation always returns a
-     * constant value.
+     * Test will timeout if secure random implementation always returns a constant value.
      */
     private void checkRandomBytes(final CryptoRandom random, final int len) {
         final byte[] bytes = new byte[len];
