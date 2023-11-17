12292f566ccd6c315bb5150d306e50e0dd8d631e
Sebb <sebb@apache.org>
2023-11-17 17:00:29 +0000

    Add test script [skip ci]

---
+++ src/docker/test_cross.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Script to test aarch64 and riscv64 under Docker
+
+set -ex
+
+cd /home/crypto # must agree with virtual mount in docker-compose.yaml
+
+mvn -V -B -ntp surefire:test "$@"
+
+# Show generated files
+find target/classes/org/apache/commons/crypto/native -type f -ls
