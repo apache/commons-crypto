5a7426b72c8f0f4fe5e6be9b101572f4e0abba14
Sebb <sebb@apache.org>
2023-11-24 22:55:41 +0000

    Documentation on distribution builds [skip ci]

---
+++ build_dist.sh
@@ -0,0 +1,30 @@
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License. See accompanying LICENSE file.
+
+# script to build native libraries
+# requires Docker and macOS
+
+mvn clean
+
+# build linux 64 bit libraries
+docker compose -f src/docker/docker-compose.yaml run crypto src/docker/build.sh
+
+# build linux 32 bit libraries
+docker compose -f src/docker/docker-compose.yaml run crypto src/docker/build_linux32.sh
+
+# build 64 bit macOS libraries
+mvn -DskipTests -Drat.skip process-classes -Dtarget.name=mac64
+mvn -DskipTests -Drat.skip process-classes -Dtarget.name=macArm64
+mvn -DskipTests -Drat.skip process-classes -Dtarget.name=mac-aarch64
+
+# package it all up
+mvn package -DskipTests
